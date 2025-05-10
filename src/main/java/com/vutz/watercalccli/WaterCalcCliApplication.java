1dd3cab36c3c4910250b376307c60364ddd3b7de
gnsals0904 <gnsals0904@naver.com>
2025-05-10 10:25:30 +0900

    Init : Project 생성

---
+++ src/main/java/com/vutz/watercalccli/WaterCalcCliApplication.java
@@ -0,0 +1,13 @@
+package com.vutz.watercalccli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaterCalcCliApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WaterCalcCliApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vutz/watercalccli/WaterCalcCliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vutz.watercalccli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaterCalcCliApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
